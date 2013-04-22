3a4203c67e78d74be45fc4f38e4bb06cf21726cc
Michele Esposito <michele@botsquare.com>
2013-04-22 17:36:47 -0500

    clean repo

--- mp11grader/mp11common.ml
+++
@@ -1,6 +0,0 @@
-type point = float * float
-
-type draw_cmd = Pixel of point
-              | Line of point * point
-              | Oval of point * point * point (* three consecutive points of
-                                                 enclosing rectangle *)
